7582acbfd6a7fd0f1bbc5cff8a68f1003a636f9b
JATIN VERMA <jv7889230967@gmail.com>
2024-05-28 15:07:01 +0530

    offset slowed

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar: React.FC = () => {
     return (
         <nav className='navbar'>
             <div className='small-nav-items sm:nav-items'>
-                <Link className='cursor-pointer' to='about' smooth={true} duration={500} >About</Link>
-                <Link className='cursor-pointer' to='skills' smooth={true} offset={-75} duration={500} >Skills</Link>
-                <Link className='cursor-pointer' to='experience' smooth={true} offset={-90} duration={500} >Experience</Link>
-                <Link className='cursor-pointer' to='contact' smooth={true} duration={500} >Contact</Link>
+                <Link className='cursor-pointer' to='about' smooth={true} duration={900} >About</Link>
+                <Link className='cursor-pointer' to='skills' smooth={true} offset={-75} duration={900} >Skills</Link>
+                <Link className='cursor-pointer' to='experience' smooth={true} offset={-90} duration={900} >Experience</Link>
+                <Link className='cursor-pointer' to='contact' smooth={true} duration={900} >Contact</Link>
             </div>
         </nav>
     );
