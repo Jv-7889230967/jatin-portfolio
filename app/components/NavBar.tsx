9c0e186ac378d92d69089ed1efd63caf86ddfa92
JATIN VERMA <jv7889230967@gmail.com>
2024-05-28 14:47:27 +0530

    eslint rules disabled

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar: React.FC = () => {
             <div className='small-nav-items sm:nav-items'>
                 <Link className='cursor-pointer' to='about' smooth={true} duration={500} >About</Link>
                 <Link className='cursor-pointer' to='skills' smooth={true} duration={500} >Skills</Link>
-                <Link className='cursor-pointer' to='experience' smooth={true} offset={-80} duration={500} >Experience</Link>
+                <Link className='cursor-pointer' to='experience' smooth={true} offset={-100} duration={500} >Experience</Link>
                 <Link className='cursor-pointer' to='contact' smooth={true} offset={-100} duration={500} >Contact</Link>
             </div>
         </nav>
