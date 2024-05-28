3f8e5965de6fa4eac73bca0e776dfa513f7c5e85
JATIN VERMA <jv7889230967@gmail.com>
2024-05-28 14:54:30 +0530

    fixed offset issue

--- app/pages/Contact/Contact.tsx
+++ app/pages/Contact/Contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='h-[45rem] w-screen flex flex-col mt-[40px] text-center'>
+    <div className='h-[25rem] w-screen flex flex-col mt-[40px] text-center'>
       <h2 className='italic text-3xl font-bold mb-[50px]'>CONTACT ME</h2>
       <div className='gap-[5px]'>
       <p className='text-xl text-neutral-800'>Email:jv7889230967@gmail.com</p>
